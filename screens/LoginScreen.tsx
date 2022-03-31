2a91e25dec2dbaf74ff674ee9916c1904478346d
brandonbrnich <bmb7qbg@virginia.edu>
2022-03-31 19:35:59 -0400

    Login stuff start

---
+++ screens/LoginScreen.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { Platform, TextInput, TouchableOpacity } from 'react-native';
+
+import { Text, View } from '../components/Themed';
+import { Buddon } from '../components/Buddons';
+import { appStyles as styles} from '../components/AppStyles';
+
+
+export default function LoginScreen() {
+  return (
+      <View>
+          <TextInput
+            style={styles.textInput}
+            placeholder='email'
+            placeholderTextColor={'#fff'}
+            keyboardType={'numeric'}
+          />
+
+          <Buddon
+            label = "Login"
+            onPress={()=>{}}
+            isSelected={false}
+          />
+      </View>
+  );
+}
