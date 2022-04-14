fba6d6e498a67112a9e1feb5af547b3435b6596d
hp-all <hpalcaine@gmail.com>
2022-04-13 22:29:21 -0400

    Login is good

--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -10,21 +10,29 @@ import Axios from "axios"
 // Components
 import { Buddon } from '../components/Buddons';
 import { FormInputError, FormField, TextField } from '../components/Form';
+import UserProfile, { thisAppUser } from '../DatabaseWrappers/Profiles';
 
-export default function Login () {
+export default function Login ({navigation}: any) {
 	// hooks that are used to change the state of the login parameters
 	const [Username, setUsername] = React.useState("");
 	const [Password, setPassword] = React.useState("");
 
 	const [LoginStatus, setLoginStatus] = React.useState("");
 
+	const deleteThisLogin = () => {
+		thisAppUser.copy(new UserProfile(0, "Admin", "AdminP_word"));
+		navigation.navigate("Root");
+
+	}
 	const login = () => {
 		Axios.post("http://localhost:3001/login", {
 			Username: Username,
 			Password: Password
 		}).then((response) => {
 			if (response.data.message == "Success"){
+				thisAppUser.copy(new UserProfile(response.data.UID, Username, Password));
 				setLoginStatus(response.data.message); // login is successful
+				navigation.navigate("Root");
 				// want to navigate to the users page from here
 			} else {
 				setLoginStatus(response.data.message);
@@ -32,30 +40,34 @@ export default function Login () {
 		})
 	}
 
-	// TODO: need to put a view in here that displays that the error message if they didn't login correctly
 	return (
 		<View style={{width: 290, alignSelf: 'center', margin: 20, padding: 20, backgroundColor: colorTheme['t_med'], borderRadius: 10}}>
 			<TextInput
-			style={[styles.textInput, {width: '100%'}]}
-			defaultValue="Username"
-			onChange={(e)=>{
-				setUsername(e.nativeEvent.text);
-			}}
+				style={[styles.textInput, {width: '100%'}]}
+				placeholder="username..."
+				placeholderTextColor={'#888'}
+				onChange={(e)=>{
+					setUsername(e.nativeEvent.text);
+				}}
 			/>
 			<TextInput
-			style={[styles.textInput, {width: '100%'}, {margin:20}]}
-			defaultValue="Password"
-			onChange={(e)=>{
-				setPassword(e.nativeEvent.text);
-			}}
+				style={[styles.textInput, {width: '100%'}, {margin:20}]}
+				placeholder="password..."
+				placeholderTextColor={'#888'}
+				onChange={(e)=>{
+					setPassword(e.nativeEvent.text);
+				}}
 			/>
 			<Buddon
 				style={[styles.submitBuddon, {margin: 15}]}
 				label = "Login"
 				onPress={login}
-				isSelected={false}
 			/>
-			<Text> {LoginStatus} </Text>
+			<Buddon
+				style={[styles.submitBuddon, {margin: 15, backgroundColor: colorTheme['t_white']}]}
+				label = "Delete This"
+				onPress={deleteThisLogin}
+			/>
 		</View>
 	)
 }
